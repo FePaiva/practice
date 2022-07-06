2d8a7aca3be360d25ccc439eed02c51bf0d6ebfa
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-07-06 13:56:26 -0700

    added some event click events

--- src/Content.js
+++ src/Content.js
@@ -8,9 +8,22 @@ const Content = () => {
       return names[int]
   }
 
+  const handleClick = () => {
+    console.log('You clicked!')
+  }
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked.`)
+  }
+  const handleClick3 = (e) => {
+    console.log(e.target.innerText)
+  }
+
   return (
       <main>
-            <p>Hello {handleNameChange()}!</p>
+            <p onDoubleClick={handleClick} >Hello {handleNameChange()}!</p>
+            <button onClick={handleClick}>Click me </button>
+            <button onClick={() => handleClick2('Dave')}>Click me </button>
+            <button onClick={(e) => handleClick3(e)}>Click me </button>
       </main>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
 const Header = () => {
+
+
+
   return (
-      <header>
+      <header  >
           <h1>Groceries List</h1>
       </header>
   )
