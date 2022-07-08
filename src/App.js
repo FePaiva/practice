f39cf95acb83f4f16e668b04c8966a52908b245c
Felippe Alves de Paiva <felippeapaiva@gmail.com>
2022-07-07 17:49:18 -0700

    added useRef to focus back to input. Search item is working.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import { useState } from 'react';
+import SearchItem from './SearchItem';
 
 function App() {
 
 const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
 const [newItem, setNewItem] = useState('')
+const [search, setSearch] = useState('')
 
 const setAndSaveItems = (newItems) => {
   setItems(newItems);
@@ -55,8 +57,12 @@ const handleSubmit = (e) => {
             setNewItem={setNewItem}  
             handleSubmit={handleSubmit}  
         />
+        <SearchItem 
+            search={search}
+            setSearch={setSearch}
+        />
         <Content  
-            items={items}
+            items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
             handleCheck={handleCheck}
             handleDelete={handleDelete}
         />
